2377d6961fae2880a073f8e40dd3eca31b794c6a
Jagatheesan <jagandecapri@gmail.com>
2023-08-06 16:23:22 +0200

    Refactor Operator Generator

--- src/operator_generator.h
+++ src/operator_generator.h
@@ -1,122 +1,16 @@
 //
 // Created by Jagan on 05/08/2023.
 //
-#include <complex>
 #include <Eigen/Dense>
 
 #ifndef TQSIM_CPP_OPERATOR_GENERATOR_H
 #define TQSIM_CPP_OPERATOR_GENERATOR_H
 
-/**
- * @brief Calculates the F matrix based on input values.
- *
- * The F matrix is a 2x2 matrix that depends on four integer parameters (a1, a2, a3, and outcome).
- * The values of these parameters determine the values in the F matrix as per the provided conditions.
- *
- * @param a1 Integer parameter a1.
- * @param a2 Integer parameter a2.
- * @param a3 Integer parameter a3.
- * @param outcome Integer parameter outcome.
- * @return Eigen::Matrix<double, 2, 2> The F matrix.
- */
-Eigen::Matrix<double, 2, 2> F(int a1, int a2, int a3, int outcome) {
-    const double inv_phi = (std::sqrt(5) - 1) / 2;  // inverse of golden number
-    Eigen::Matrix<double, 2, 2> f_matrix;
+class OperatorGeneratorInterface {
+public:
+    virtual Eigen::Matrix<double, 2, 2> F(int, int, int, int) = 0;
 
-    // a1 + a2 + a3 + outcome = 4
-    if (a1 + a2 + a3 + outcome == 4) {
-        f_matrix << inv_phi, std::sqrt(inv_phi),
-                std::sqrt(inv_phi), -inv_phi;
-    }
-        // a1 + a2 + a3 + outcome = 3
-    else if (a1 + a2 + a3 + outcome == 3) {
-        f_matrix << 0, 0,
-                0, 1;
-    }
-        // a1 + a2 + a3 + outcome = 2
-    else if (a1 + a2 + a3 + outcome == 2) {
-        if (a1 + a2 == 2) {
-            f_matrix << 0, 1,
-                    0, 0;
-        } else if (a2 + a3 == 2) {
-            f_matrix << 0, 0,
-                    1, 0;
-        } else if (a1 + a3 == 2) {
-            f_matrix << 0, 0,
-                    0, 1;
-        } else if (a3 + outcome == 2) {
-            f_matrix << 0, 1,
-                    0, 0;
-        } else if (a1 + outcome == 2) {
-            f_matrix << 0, 0,
-                    1, 0;
-        } else if (a2 + outcome == 2) {
-            f_matrix << 0, 0,
-                    0, 1;
-        }
-    }
-        // a1 + a2 + a3 + outcome = 1 or 0
-    else if (a1 + a2 + a3 + outcome <= 1) {
-        f_matrix << 1, 0,
-                0, 0;
-    }
-
-    return f_matrix;
-}
-
-/**
- * @brief Calculates the R matrix based on input values.
- *
- * The R matrix is a 2x2 matrix that depends on two integer parameters (a1 and a2).
- * The values of these parameters determine the values in the R matrix as per the provided conditions.
- *
- * @param a1 Integer parameter a1.
- * @param a2 Integer parameter a2.
- * @return Eigen::Matrix<std::complex<double>, 2, 2> The R matrix.
- */
-Eigen::Matrix<std::complex<double>, 2, 2> R(int a1, int a2) {
-    using complex_t = std::complex<double>;
-    Eigen::Matrix<complex_t, 2, 2> r_matrix;
-
-    if (a1 + a2 == 2) {
-        const double pi = std::acos(-1);
-        complex_t exp_val1 = std::exp(complex_t(0.0, -4 * pi / 5.0));
-        complex_t exp_val2 = std::exp(complex_t(0.0, 3 * pi / 5.0));
-        r_matrix << exp_val1, complex_t(0, 0),
-                complex_t(0, 0), exp_val2;
-    } else {
-        r_matrix << complex_t(1, 0), complex_t(0, 0),
-                complex_t(0, 0), complex_t(1, 0);
-    }
-
-    return r_matrix;
-}
-
-// Define the B function (equivalent to Python implementation)
-Eigen::Matrix<std::complex<double>, 2, 2> B(int a0, int a1,
-                                            int a2, int outcome) {
-    /*
-    Braid matrix
-
-    Parameters:
-        a0: Matrix representing a0
-        a1: Matrix representing a1
-        a2: Matrix representing a2
-        outcome: Matrix representing outcome
-
-    Returns:
-        b_matrix: The braid matrix computed using Eigen.
-    */
-
-    // Compute the intermediate matrices
-    Eigen::Matrix<double, 2, 2> f_result = F(a0, a1, a2, outcome);
-    Eigen::Matrix<std::complex<double>, 2, 2> r_result = R(a1, a2);
-    Eigen::Matrix<double, 2, 2> f_transpose_conjugate_result = F(a0, a2, a1, outcome).transpose().conjugate();
-
-    // Compute the braid matrix using Eigen matrix operations
-    Eigen::Matrix<std::complex<double>, 2, 2> b_matrix = f_result * r_result * f_transpose_conjugate_result;
-
-    return b_matrix;
-}
+    virtual Eigen::Matrix<std::complex<double>, 2, 2> R(int, int) = 0;
+};
 
 #endif //TQSIM_CPP_OPERATOR_GENERATOR_H
--- test/test_operator_generator.cpp
+++ test/test_operator_generator.cpp
@@ -2,10 +2,11 @@
 // Created by Jagan on 06/08/2023.
 //
 #include <iostream>
+#include <cmath>
 #include <Eigen/Dense>
 #include <gtest/gtest.h>
-#include "gmock/gmock.h"
-#include <operator_generator.h>
+#include <operator_generator.cpp>
+
 
 /**
  * @brief Custom comparison function for complex numbers.
@@ -16,14 +17,15 @@
  * @return true if both real and imaginary parts are within the tolerance, false otherwise.
  */
 bool complexIsApprox(const std::complex<double> &a, const std::complex<double> &b, double tol = 1e-6) {
-    return std::abs(a.real() - b.real()) < tol && std::abs(a.imag() - b.imag()) < tol;
+    return abs(a.real() - b.real()) < tol && abs(a.imag() - b.imag()) < tol;
 }
 
 // Test case for the F function
 TEST(FFunctionTest, TestCases) {
+    OperatorGenerator operator_generator = OperatorGenerator();
     // Test case 1: a1 + a2 + a3 + outcome = 4
     int a1_case1 = 1, a2_case1 = 1, a3_case1 = 1, outcome_case1 = 1;
-    Eigen::Matrix<double, 2, 2> result_case1 = F(a1_case1, a2_case1, a3_case1, outcome_case1);
+    Eigen::Matrix<double, 2, 2> result_case1 = operator_generator.F(a1_case1, a2_case1, a3_case1, outcome_case1);
     Eigen::Matrix<double, 2, 2> expected_case1;
     const double inv_phi = (std::sqrt(5) - 1) / 2;
     expected_case1 << inv_phi, std::sqrt(inv_phi),
@@ -32,7 +34,7 @@ TEST(FFunctionTest, TestCases) {
 
     // Test case 2: a1 + a2 + a3 + outcome = 3
     int a1_case2 = 0, a2_case2 = 1, a3_case2 = 2, outcome_case2 = 0;
-    Eigen::Matrix<double, 2, 2> result_case2 = F(a1_case2, a2_case2, a3_case2, outcome_case2);
+    Eigen::Matrix<double, 2, 2> result_case2 = operator_generator.F(a1_case2, a2_case2, a3_case2, outcome_case2);
     Eigen::Matrix<double, 2, 2> expected_case2;
     expected_case2 << 0, 0,
             0, 1;
@@ -43,9 +45,10 @@ TEST(FFunctionTest, TestCases) {
 
 // Test case for the R function
 TEST(RFunctionTest, TestCases) {
+    OperatorGenerator operator_generator = OperatorGenerator();
     // Test case 1: a1 + a2 = 2
     int a1_case1 = 1, a2_case1 = 1;
-    Eigen::Matrix<std::complex<double>, 2, 2> result_case1 = R(a1_case1, a2_case1);
+    Eigen::Matrix<std::complex<double>, 2, 2> result_case1 = operator_generator.R(a1_case1, a2_case1);
     Eigen::Matrix<std::complex<double>, 2, 2> expected_case1;
     const double pi = std::acos(-1);
     std::complex<double> exp_val1 = std::exp(std::complex<double>(0.0, -4 * pi / 5.0));
@@ -59,7 +62,7 @@ TEST(RFunctionTest, TestCases) {
 
     // Test case 2: a1 + a2 != 2
     int a1_case2 = 0, a2_case2 = 1;
-    Eigen::Matrix<std::complex<double>, 2, 2> result_case2 = R(a1_case2, a2_case2);
+    Eigen::Matrix<std::complex<double>, 2, 2> result_case2 = operator_generator.R(a1_case2, a2_case2);
     Eigen::Matrix<std::complex<double>, 2, 2> expected_case2;
     expected_case2 << std::complex<double>(1, 0), std::complex<double>(0, 0),
             std::complex<double>(0, 0), std::complex<double>(1, 0);
@@ -71,24 +74,19 @@ TEST(RFunctionTest, TestCases) {
     // Add more test cases as needed...
 }
 
-Eigen::Matrix<double, 2, 2> F_Mock(int a1, int a2, int a3, int outcome) {
-    Eigen::Matrix<double, 2, 2> f_matrix;
-    f_matrix << 0, 0,
-            0, 1;
-    return f_matrix;
-}
-
 // Test case for the B function
 TEST(BFunctionTest, TestCases) {
-    // This declares a lambda, which can be called just like a function
-    Eigen::Matrix<double, 2, 2> (*originalF)(int, int, int, int) = F;
-    F = F_Mock;
+    OperatorGenerator operator_generator = OperatorGenerator();
     // Test case 1: a1 + a2 + a3 + outcome = 4
     int a0 = 1, a1 = 1, a2 = 1, outcome = 1;
-    Eigen::Matrix<std::complex<double>, 2, 2> b_matrix = B(a0, a1, a2, outcome);
+    Eigen::Matrix<std::complex<double>, 2, 2> result_case1 = operator_generator.B(a0, a1, a2, outcome);
     Eigen::Matrix<std::complex<double>, 2, 2> expected_case1;
-    const double pi = std::acos(-1);
-    std::complex<double> exp_val1 = std::exp(std::complex<double>(0.0, -4 * pi / 5.0));
-    std::complex<double> exp_val2 = std::exp(std::complex<double>(0.0, 3 * pi / 5.0));
-    F = originalF;
+    expected_case1 << std::complex<double>(-0.5, 0.36327126400268051), std::complex<double>(-0.24293413587832285,
+                                                                                            -0.74767439061061047),
+            std::complex<double>(-0.24293413587832285, -0.74767439061061047), std::complex<double>(-0.61803398874989468,
+                                                                                                   5.5511151231257827E-17);
+    ASSERT_TRUE(complexIsApprox(result_case1(0, 0), expected_case1(0, 0)) &&
+                complexIsApprox(result_case1(0, 1), expected_case1(0, 1)) &&
+                complexIsApprox(result_case1(1, 0), expected_case1(1, 0)) &&
+                complexIsApprox(result_case1(1, 1), expected_case1(1, 1)));
 }
