ce90a68d917c7f285cef6559edaac9a411d36ada
Jagatheesan <jagandecapri@gmail.com>
2023-09-09 20:11:00 +0200

    Update main.cpp with code to generate braiding DD

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+#include "dd/DDDefinitions.hpp"
+#include "dd/Export.hpp"
+#include "dd/Package.hpp"
 #include "src/Circuit.cpp"
 
 #include <chrono>
+#include <cmath>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,8 +14,8 @@ int main(int /*unused*/, char** /*unused*/) {
   cout << "Hello, from tqsim-cpp!\n";
   // H_2
   Sequence hadSequence2 = {{4, 2}, {5, 2},  {4, -2}, {5, -2}, {4, 2},
-                             {5, 4}, {4, -2}, {5, 2},  {4, 2},  {5, -2},
-                             {4, 2}, {5, -2}, {4, 4}};
+                           {5, 4}, {4, -2}, {5, 2},  {4, 2},  {5, -2},
+                           {4, 2}, {5, -2}, {4, 4}};
 
   // CNOT_2->1
   Sequence cnotSequence = {
@@ -52,13 +57,29 @@ int main(int /*unused*/, char** /*unused*/) {
 
   // Start the timer
   auto start = std::chrono::high_resolution_clock::now();
+  const auto nrQubits = 2U;
 
-  Circuit circuit = Circuit(2, 3);
+  Circuit circuit = Circuit(nrQubits, 3);
   Eigen::VectorXcd initSequence(13);
   initSequence << 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
   circuit.initialize(initSequence);
   circuit.braidSequence(hadSequence2);
   circuit.braidSequence(cnotSequence);
+
+  std::vector<Eigen::MatrixXcd> const braidingOperators =
+      circuit.getBraidingOperators();
+
+  // Define the range for rows and columns you want to select
+  size_t const startRow = 1; // First row
+  auto const endRow =
+      static_cast<size_t>(pow(2, nrQubits)); // Fifth row (0-based indexing)
+  int const startCol = 1;                    // First column
+  auto const endCol =
+      static_cast<size_t>(pow(2, nrQubits)); // Fifth column (0-based indexing)
+
+  std::cout << "endRow: " << endRow << '\n';
+  std::cout << "endCol: " << endCol << '\n';
+
   circuit.measure();
   int const shots = 10000;
   Result const res = circuit.run(shots);
@@ -74,5 +95,55 @@ int main(int /*unused*/, char** /*unused*/) {
   for (const auto& count : res.countsDict) {
     std::cout << count.first << ": " << count.second << '\n';
   }
+
+  for (int index = 0; index < static_cast<int>(braidingOperators.size());
+       ++index) {
+
+    // Create a std::ostringstream object
+    std::ostringstream oss;
+
+    // Format the string
+    oss << std::to_string(nrQubits) << "_qubit_"
+        << "sigma_" << index + 1 << "_operator.dot";
+
+    // Get the formatted string
+    std::string const formattedString = oss.str();
+
+    // Print the formatted string
+    std::cout << formattedString << '\n';
+
+    // Select the sub-matrix
+    Eigen::MatrixXcd complexMatrix =
+        braidingOperators[static_cast<size_t>(index)]
+            .block(startRow, startCol, endRow - startRow + 1,
+                   endCol - startCol + 1)
+            .cast<std::complex<double>>();
+
+    std::cout << "Braiding operator: \n"
+              << braidingOperators[static_cast<uint64_t>(index)] << '\n';
+
+    // Initialize a std::vector<std::vector<std::complex<double>>> with the same
+    // dimensions as the Eigen matrix
+    dd::CMat sigmaMatrix(static_cast<size_t>(complexMatrix.rows()),
+                         dd::CVec(static_cast<size_t>(complexMatrix.cols())));
+
+    // Copy elements from the Eigen matrix to the
+    // std::vector<std::vector<std::complex<double>>>
+    for (int i = 0; i < complexMatrix.rows(); ++i) {
+      for (int j = 0; j < complexMatrix.cols(); ++j) {
+        auto row = static_cast<size_t>(i);
+        auto col = static_cast<size_t>(j);
+        sigmaMatrix[row][col] = complexMatrix(i, j);
+        std::cout << sigmaMatrix[row][col] << " ";
+      }
+      std::cout << '\n';
+    }
+    std::cout << '\n';
+
+    const auto dd = std::make_unique<dd::Package<>>(nrQubits);
+    const auto matDD = dd->makeDDFromMatrix(sigmaMatrix);
+    export2Dot(matDD, formattedString, true, true, false, false, true);
+  }
+
   return 0;
 }
